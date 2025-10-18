00f531da3670dcd6a8cac240943dd0c774a00103
Arié Selinger <arie.selinger@gmail.com>
2025-10-18 14:33:06 +0200

    Add linear example script

---
+++ scripts/linear_example.py
@@ -0,0 +1,25 @@
+import numpy as np
+from tinytorch.linear import Linear
+
+batch_size = 12
+d1 = 10
+d2 = 5
+d3 = 4
+d4 = 17
+
+x = np.random.randn(batch_size, d1)
+
+l1 = Linear(d1, d2)
+l2 = Linear(d2, d3)
+l3 = Linear(d3, d4)
+
+y = l1(x)
+z = l2(y)
+w = l3(z)
+
+grad_w = np.random.randn(batch_size, d4)
+grad_z = l3.backward(grad_w)
+grad_y = l2.backward(grad_z)
+grad_x = l1.backward(grad_y)
+
+l2.zero_grad()
