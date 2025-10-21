0a238f6109bf3b23bb8fad62d8308f8069279f4a
Arié Selinger <arie.selinger@gmail.com>
2025-10-21 14:33:06 +0200

    Add Sequence module

---
+++ tinytorch/sequence.py
@@ -0,0 +1,26 @@
+import numpy as np
+from typing import Sequence
+from tinytorch.module import Module
+from tinytorch.parameter import Parameter
+
+
+class Sequential(Module):
+  def __init__(self, layers: Sequence[Module]) -> None:
+    self.layers = layers
+
+  def forward(self, x: np.ndarray) -> np.ndarray:
+    for m in self.layers:
+      x = m.forward(x)
+    return x
+
+  def backward(self, grad_out: np.ndarray):
+    grad = grad_out
+    for m in reversed(self.layers):
+      grad = m.backward(grad)
+    return grad
+
+  def parameters(self) -> Sequence[Parameter]:
+    params: list[Parameter] = []
+    for m in self.layers:
+      params += m.parameters()
+    return params
