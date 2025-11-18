d630788d0253b24521dc1ea5e8ee6a2a725bc6e3
Arié Selinger <arie.selinger@gmail.com>
2025-11-18 14:27:11 +0100

    Update MSELoss docstring

--- tinytorch/criteria/mse.py
+++ tinytorch/criteria/mse.py
@@ -7,26 +7,26 @@ from tinytorch.parameter import Parameter
 
 class MSELoss(CriterionModule):
   _diff: np.ndarray | None
-  _n: int | None
+  _size: int | None
 
   def forward(self, y_pred: np.ndarray, y_true: np.ndarray) -> np.ndarray:
     """
     Output:
-      <X-Y,X-Y>_F / N = tr((X-Y).T @ (X-Y)) / N
+      L(x,y) = 1 / N * <X-Y,X-Y>_F
 
     Gradient:
-      grad_X =
+      grad_X = 2 / N * (X - Y)
     """
     if y_pred.shape != y_true.shape:
       raise RuntimeError("y_true and y_pred should have the same shapes")
-    self._n = y_pred.size
+    self._size = y_pred.size
     self._diff = y_pred - y_true
-    return np.array(np.sum(self._diff**2) / self._n)
+    return np.array(np.sum(self._diff**2) / self._size)
 
   def backward(self, grad_out: np.ndarray = np.array(1.0)) -> np.ndarray:
-    if self._n is None or self._diff is None:
+    if self._size is None or self._diff is None:
       raise ForwardNotCalledError()
-    return grad_out * 2 * self._diff / self._n
+    return grad_out * 2 * self._diff / self._size
 
   def parameters(self) -> Sequence[Parameter]:
     return []
