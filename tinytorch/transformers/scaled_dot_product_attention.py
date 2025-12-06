Make causal mask an optional flag and fix for any query size

tinytorch/transformers/scaled_dot_product_attention.py
@@ -9,25 +9,39 @@ from tinytorch.parameter import Parameter
 
 
 class ScaledDotProductAttention(ThreeInputModule):
-  def __init__(self, d_k: int) -> None:
+  def __init__(self, causal_mask: bool) -> None:
     """
-    Docstring for __init__
+    ScaledDotProductAttention
 
     Args:
-      d_k: dim of keys and queries
+      causal_mask: if true, apply causal mask so that each query can only attend its previous keys.
+                   The mask aligns to the bottom-right, so the last query attends to all keys,
+                   second-to-last attends to all but the last key, etc.
+                    [[0 0 -inf -inf -inf]
+                     [0 0    0 -inf -inf]
+                     [0 0    0    0 -inf] <- second to last query
+                     [0 0    0    0   0]] <- last query
     """
     self._matmul1 = MatMul()
     self._softmax = Softmax()
-    self._scale: np.floating = 1 / np.sqrt(d_k)
     self._matmul2 = MatMul()
+    self._causal_mask = causal_mask
 
   def forward(self, Q: np.ndarray, K: np.ndarray, V: np.ndarray) -> np.ndarray:  # noqa: N803
-    seq_len = Q.shape[-2]
-    mask = np.triu(-np.inf * np.ones((seq_len, seq_len)), k=1)
+    num_queries = Q.shape[-2]
+    num_keys = K.shape[-2]
+
+    mask = None
+    if self._causal_mask and num_queries >= 1:
+      mask = np.triu(-np.inf * np.ones((num_queries, num_keys)), k=num_keys - num_queries + 1)
+
+    scale = 1 / np.sqrt(K.shape[-1])
+    self._scale: np.floating = scale
 
     scores = self._matmul1(Q, K.swapaxes(-1, -2))
-    scores = scores * self._scale
-    scores = scores + mask
+    scores = scores * scale
+    if mask is not None:
+      scores = scores + mask
     attention = self._softmax(scores)
     output = self._matmul2(attention, V)
     return output